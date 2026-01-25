aab9bab1c3b8f1b770f5204f1c44f53935f85f61
Emilio Barrera <barrera_emilio@yahoo.es>
2026-01-25 20:55:14 +0000

    Add project files.

---
+++ GreenBowlFoodsSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GreenBowlFoodsSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
